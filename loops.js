a94a97c5f93053c269fd2642c0a8be819e666073
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-05 05:14:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ for (let i = 0; i < 25; i++){
   if (typeOf(i) === number){
     array.push("I am " + i + " strange loop.");
   } else {
-    array.push(`I am ${i} strange loop.``)
+    array.push(`I am ${i} strange loop.`);
   }
   return array;
 }
