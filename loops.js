351502fac3da808afa9d87474d3e13e395198b30
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-05 05:34:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,3 +10,13 @@ function forLoop(array){
   }
   return array;
 }
+
+
+function whileLoop(number){
+  let countdown = number;
+  while (countdown >= 0 ){
+    console.log(countdown);
+    countdown--;
+  } return "done";
+  }
+
